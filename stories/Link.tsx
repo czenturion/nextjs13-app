76a6bec8aad1f424f29b3b68018a59983c9d01f5
czntrn <czenturijon@gmail.com>
2023-07-22 17:39:47 +0300

    sb tests

--- stories/Link.tsx
+++ stories/Link.tsx
@@ -3,33 +3,24 @@ import './link.css';
 
 interface LinkProps {
     primary?: boolean
-    backgroundColor?: string
     size?: 'small' | 'medium' | 'large'
     label: string
-    path: '#'
 }
 
 export const Link = ({
                          primary = false,
-                         backgroundColor = 'blue',
                          size = 'medium',
                          label,
-                         path = '#',
                          ...props
                      }: LinkProps) => {
     const mode = primary ? 'storybook-link--primary' : 'storybook-link--secondary';
     return (
         <a
             className={['storybook-link', `storybook-link--${size}`, mode].join(' ')}
-            href={path}
+            href='#'
             {...props}
         >
             {label}
-            <style jsx>
-                {`link {
-                    background-color: ${backgroundColor};
-                }`}
-            </style>
         </a>
     )
 }
