bb9f1def7eaf447978db1a1287f4df1993a25dc7
czntrn <czenturijon@gmail.com>
2023-07-24 17:07:58 +0300

    sb sign up form collated

--- stories/Form.tsx
+++ stories/Form.tsx
@@ -1,15 +1,14 @@
 import React from "react";
 import "./form.css";
+import Image from "next/image";
 
 interface FormProps {
     primary?: boolean
-    size?: 'small' | 'medium' | 'large'
     onClick: (data) => void
 }
 
 export const Form = ({
                          primary = true,
-                         size = 'medium',
                          onClick
                      }: FormProps) => {
     const mode = primary ? 'storybook-link--primary' : 'storybook-link--secondary';
@@ -23,16 +22,34 @@ export const Form = ({
             className="background"
         >
             <form
-                className={['storybook-form', `storybook-form--${size}`, mode].join(' ')}
+                className={['storybook-form', mode].join(' ')}
             >
-                <h3>Login</h3>
-                <input className="email" type="text" placeholder="Login"/>
-                <input className="password" type="text" placeholder="Password"/>
-                <div className="checkbox">
-                    <input type="checkbox" id="rememberMe"/>
-                    <label htmlFor="rememberMe">Remember me</label>
+                <h2>Sign Up</h2>
+                <div className="icons">
+                    <Image src="google.svg" alt="google" width="36" height="36"/>
+                    <Image src="git.svg" alt="git" width="36" height="36"/>
                 </div>
-                <button onClick={onClick}>Log In</button>
+                <div className="inputs">
+                    <label htmlFor="username">Username</label>
+                    <input className="username" type="text" placeholder="Login" id="login"/>
+                </div>
+                <div className="inputs">
+                    <label htmlFor="email">Email</label>
+                    <input className="email" type="text" placeholder="Email" id="password"/>
+                </div>
+                <div className="inputs password">
+                    <label htmlFor="password">Password</label>
+                    <input className="password" type="password" placeholder="******************" id="password"/>
+                    <img src="eye-outline.svg" alt="visible" width="24" height="24"/>
+                </div>
+                <div className="inputs password">
+                    <label htmlFor="password">Password confirmation</label>
+                    <input className="password" type="password" placeholder="******************" id="password"/>
+                    <img src="eye-outline.svg" alt="visible" width="24" height="24"/>
+                </div>
+                <button onClick={onClick} className="submit">Sign Up</button>
+                <p className="signIn-title">Do you have an account?</p>
+                <button onClick={onClick} className="signIn">Sign In</button>
             </form>
         </div>
     )
